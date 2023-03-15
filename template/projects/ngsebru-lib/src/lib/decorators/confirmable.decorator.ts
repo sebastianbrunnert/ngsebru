5fae0a04e2c87a6084283ced71c28b3912108b62
Sebastian Brunnert <se.brunnert@gmail.com>
2023-03-15 11:11:42 +0100

    Close Confirmable after Submit

--- template/projects/ngsebru-lib/src/lib/decorators/confirmable.decorator.ts
+++ template/projects/ngsebru-lib/src/lib/decorators/confirmable.decorator.ts
@@ -4,10 +4,10 @@ export function Confirmable(body: String): Function {
     return (target: Function, propertyKey: string, descriptor: PropertyDescriptor) => {
         const originalMethod = descriptor.value;
         descriptor.value = function (...args: any[]) {
-            new NgSModal().setTitle("CONFIRMABLE_HEADLINE")
-                .addButton(new NgSModalButton("CONFIRMABLE_SUBMIT", "red-500", "red-700", () => originalMethod.apply(this, args)))
+            const modal = new NgSModal().setTitle("CONFIRMABLE_HEADLINE")
+                .addButton(new NgSModalButton("CONFIRMABLE_SUBMIT", "red-500", "red-700", () => { originalMethod.apply(this, args); modal.close() }))
                 .setText(body)
-                .open()
+            modal.open();
         };
         return descriptor;
     }
